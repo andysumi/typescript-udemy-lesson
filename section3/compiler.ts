4fa0a760c83e72132c8df6acd36106fc69560730
andysumi <andysumi@ruby.net.tokai-u.jp>
2020-04-06 09:25:05 +0900

    section3

---
+++ section3/compiler.ts
@@ -0,0 +1,8 @@
+// watchモード
+// tsc index.ts --watch
+
+// tsc --init
+// tsconfig.json 設定ファイルを作成する
+
+let hello = 'hello!!';
+console.log(hello.toUpperCase());
---
+++ section3/index.ts
@@ -0,0 +1,4 @@
+function tasu(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log(tasu(1, 2));
